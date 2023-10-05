3dc8543604621d4045c9f837735fc45f1c7031f5
Iamneo <techadmin@iamneo.ai>
2023-10-05 04:15:45 +0000

    Backup Commit

--- springapp/service/ApiService.java
+++ springapp/service/ApiService.java
@@ -1,32 +1,21 @@
-package com.examly.springapp.controller;
+package com.examly.springapp.service;
 
 import com.examly.springapp.model.Laptop;
-import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController
-@RequestMapping("/laptops")
-public class LaptopController {
+public class ApiService {
     private List<Laptop> laptops = new ArrayList<>();
 
-    @PostMapping
-    public boolean addLaptop(@RequestBody Laptop laptop) {
-        // Initialize the list if it's not already created (assuming it's declared as an instance variable).
-        if (laptops == null) {
-            laptops = new ArrayList<>();
-        }
-    
-        // Add the provided laptop to the list.
+    public boolean addLaptop(Laptop laptop) {
+        // Implement the logic to add a new laptop to the list
+        // You can return true if the laptop is successfully added, or false if there's an issue.
         laptops.add(laptop);
-    
-        // Return true to indicate that the laptop was successfully added.
-        return true;
+        return true; // Modify this logic accordingly.
     }
-    
-    @GetMapping("/{laptopid}")
-    public Laptop getLaptopById(@PathVariable int laptopid) {
+
+    public Laptop getLaptopById(int laptopid) {
         // Implement the logic to retrieve a laptop by its ID
         // You should search the list of laptops and return the corresponding laptop object.
         for (Laptop laptop : laptops) {
@@ -37,7 +26,6 @@ public class LaptopController {
         return null; // Return null if the laptop with the specified ID is not found.
     }
 
-    @GetMapping
     public List<Laptop> getAllLaptops() {
         // Implement the logic to retrieve a list of all laptops
         return laptops; // Return the list of all laptops stored in the "laptops" list.
