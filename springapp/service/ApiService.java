812d5aa7f2c168371bcd5bac1fab61e1f3748f15
Iamneo <techadmin@iamneo.ai>
2023-10-05 04:35:45 +0000

    Backup Commit

--- springapp/service/ApiService.java
+++ springapp/service/ApiService.java
@@ -1,3 +1,33 @@
+package com.examly.springapp.service;
+
+import com.examly.springapp.model.Laptop;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ApiService {
-    
+    private List<Laptop> laptops = new ArrayList<>();
+
+    public boolean addLaptop(Laptop laptop) {
+        // Implement the logic to add a new laptop to the list
+        // You can return true if the laptop is successfully added, or false if there's an issue.
+        laptops.add(laptop);
+        return true; // Modify this logic accordingly.
+    }
+
+    public Laptop getLaptopById(int laptopid) {
+        // Implement the logic to retrieve a laptop by its ID
+        // You should search the list of laptops and return the corresponding laptop object.
+        for (Laptop laptop : laptops) {
+            if (laptop.getLaptopid() == laptopid) {
+                return laptop;
+            }
+        }
+        return null; // Return null if the laptop with the specified ID is not found.
+    }
+
+    public List<Laptop> getAllLaptops() {
+        // Implement the logic to retrieve a list of all laptops
+        return laptops; // Return the list of all laptops stored in the "laptops" list.
+    }
 }
